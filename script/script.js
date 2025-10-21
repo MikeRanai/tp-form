a8eda937904d9c7275d8fee9178dcac807780784
Mike Ranai <ranaimike@gmail.com>
2025-10-21 20:26:06 +0400

    Correction du scrip.js et ajout de la class form-control à l'input date de naissance

--- script/script.js
+++ script/script.js
@@ -1,11 +1,20 @@
+function verifBirthDay() {
+    let dateNaissance = document.getElementById("birthDay").value;
+    
+    // Vérifier si le champ date est vide
+    if (!dateNaissance) {
+        console.log("Erreur : Veuillez entrer une date de naissance.");
+        return;
+    }
 
-function verifBirthDay(){
-    let dateNaissance = document.getElementById("birthDay").value; 
-    let dateToday = Date.now();
-    if (dateNaissance<dateToday){
-        console.log("Date de naissance valide")
-    }else{
-        console.log("error")
+    // Convertir la chaîne de date en objet Date
+    let birthDate = new Date(dateNaissance);
+    let dateToday = new Date(); // Date actuelle
+
+    // Vérifier si la date de naissance est valide et antérieure à aujourd'hui
+    if (birthDate < dateToday) {
+        console.log("Date de naissance valide");
+    } else {
+        console.log("Erreur : La date de naissance doit être antérieure à aujourd'hui.");
     }
 }
-
